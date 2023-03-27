db6315b6a77ef720543b2f6f647084121bb40997
XC <xca130@sfu.ca>
2023-03-27 00:03:16 -0700

    Update some base UI

--- app/src/main/java/com/example/expensestracker/LoginActivity.java
+++ app/src/main/java/com/example/expensestracker/LoginActivity.java
@@ -5,9 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -23,6 +25,7 @@ public class LoginActivity extends AppCompatActivity {
 
         preferences = getSharedPreferences("MyPreferences", MODE_PRIVATE);
         editor = preferences.edit();
+        Log.d("LoginActivity", "Logging enabled");
 
         if(preferences.contains("saved_name")){
             Intent intent = new Intent(LoginActivity.this, MainActivity.class);
@@ -32,6 +35,28 @@ public class LoginActivity extends AppCompatActivity {
             name = findViewById(R.id.usernameEditText);
             psd = findViewById(R.id.passwordEditText);
             loginBtn = findViewById(R.id.loginBtn);
+
+//            String savedUsername = preferences.getString("saved_name", "");
+//            String savedPassword = preferences.getString("saved_password", "");
+//
+//            Log.d("LoginActivity", "savedUsername: " + savedUsername);
+//            Log.d("LoginActivity", "savedPassword: " + savedPassword);
+//
+//            if (savedUsername.isEmpty() || savedPassword.isEmpty()) {
+//                // Username or password is missing
+//                // Show an error message
+//                Toast.makeText(this, "Please enter your username and password", Toast.LENGTH_SHORT).show();
+//
+//                // Disable the login button
+//                loginBtn.setEnabled(false);
+//                Log.d("LoginActivity", "Login button disabled");
+//            } else {
+//                // Username and password are both present
+//                // Enable the login button
+//                loginBtn.setEnabled(true);
+//                Log.d("LoginActivity", "Login button enabled");
+//            }
+
             loginBtn.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -40,6 +65,8 @@ public class LoginActivity extends AppCompatActivity {
                     editor.putString("saved_name", my_name);
                     editor.putString("saved_password", my_password);
                     editor.commit();
+
+
                     Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                     startActivity(intent);
                 }
